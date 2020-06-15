30c425928bdf8de37ff6a3837acea663eea89919
Nicolefonseca98 <37601556+Nicolefonseca98@users.noreply.github.com>
2020-06-14 18:17:49 -0600

    creación proyecto

---
+++ src/main/java/ucr/ac/cr/project/ProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringApplication.class, args);
+    }
+
+}
